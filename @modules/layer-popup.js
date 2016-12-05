d93f8dcee150da6d6998deb04638b1eebed5454c
msminsu <msminsu@nate.com>
2016-12-05 17:57:40 +0900

    update

--- @modules/layer-popup.js
+++ @modules/layer-popup.js
@@ -1,6 +1,3 @@
-/**
- * Created by msminsu on 2016-11-30.
- */
 
 minsu.layerPopup = (function($){
     var layerSection = $('#layer-section');
@@ -16,6 +13,75 @@ minsu.layerPopup = (function($){
         layerDataStore['#'+$(html).attr('id')] = $(html);
     }
 
+    function open(template){
+        addData(template);
+        showLayer(false, '#'+$(template).attr('id'));
+    }
+
+    function isOpen(){
+        return lqyerSection.find('.layer').length > 0;
+    }
+
+    function close(hideId){
+        hideLayer(false, hideId);
+    }
+
+    function showLayer(e, layerId) {
+        var showId;
+        if(e){
+            e.preventDefault();
+            showId = minsu.helper.getBtnTarget(e.target);
+        }else{
+            showId = layerId;
+        }
+
+        if(openList.find(showId) > -1){
+            return;
+        }
+
+        layerSection.append(layerDataStore[showId]).addClass('active');
+        $(layerDataStore[showId]).addClass('on').focus();
+        openList.add(showId);
+        openHistory.add($(e.target));
+    }
+
+    function hideLayer(e, hideID){
+        hideID = hideId || "#" + $(this).parent().attr('id');
+        openList.remove(hideID);
+        $(hideID).remove();
+
+        if(!isOpen()){
+            layerSection.removeClass('active');
+        }
+        if(e){
+            e.preventDefault();
+            openHistory.get().focus();
+        }
+    }
+
+    function initOpen(){
+        $('[data-ui-layer-open]').each(function(){
+            if($(this).attr('data-ui-layer-open') === 'false'){
+                return;
+            }
+            $(this).trigger('click.minsu.layerPopup');
+        });
+    }
+
+    layerSection.find('.layer').each(function () {
+        addData(this);
+        $(this).remove();
+    });
+
+    $(document).on('click.minsu.layerPopup','[data-ui-layer-open]',showLayer);
+    $(document).on('click.minsu.layerPopup','.btn-layer-close', hideLayer);
+
+    initOpen();
+
+    return{
+        open: open,
+        close: close
+    }
 
 
 })(window.jQuery);
--- spinbox/app.js
+++ spinbox/app.js
@@ -1,19 +1,19 @@
 
 (function($){
-    $('.spin-box').each(function(){
-       var obj = $(this),
-           btnMinus = obj.find('.btn-minus'),
-           btnPlus = obj.find('.btn-plus'),
-           input = obj.find('input');
+  $('.spin-box').each(function(){
+   var obj = $(this),
+   btnMinus = obj.find('.btn-minus'),
+   btnPlus = obj.find('.btn-plus'),
+   input = obj.find('input');
 
-        btnPlus.on('click',function(){
-           input.val(+input.val() + 1);
-        });
+   btnPlus.on('click',function(){
+     input.val(+input.val() + 1);
+   });
 
-        btnMinus.on('click',function(){
-            input.val(+input.val()-1 > 0? +input.val() -1 : 0);
-        })
-    });
+   btnMinus.on('click',function(){
+    input.val(+input.val()-1 > 0? +input.val() -1 : 0);
+  })
+ });
 })(window.jQuery);
 
 /*(function(){
